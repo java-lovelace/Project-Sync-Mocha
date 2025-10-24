fefaea1f5d32bc70c5c9620ac827eb61a898bac9
andresfelipe07b <bolanosandres43@gmail.com>
2025-10-24 07:47:40 -0500

    feat: create structure

---
+++ src/main/java/com/mocha/projectsyncmocha/ProjectSyncMochaApplication.java
@@ -0,0 +1,13 @@
+package com.mocha.projectsyncmocha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSyncMochaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSyncMochaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mocha/projectsyncmocha/ProjectSyncMochaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mocha.projectsyncmocha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSyncMochaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
